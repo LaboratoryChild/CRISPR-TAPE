debcd3321042a9eb63a6353d9e73951f8364b00a
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2022-04-01 09:45:23 +0100

    add icon to automatic build

--- tape_build-runner.py
+++ tape_build-runner.py
@@ -1,5 +1,5 @@
 import subprocess
 
 if __name__ == '__main__':
-    build_command = 'pyinstaller --onefile -w -n CRISPR-TAPE CRISPR_TAPE/interface.py'
+    build_command = 'pyinstaller --onefile -c --icon=icon.ico -p . --python-option v -n CRISPR-TAPE CRISPR_TAPE/interface.py'
     subprocess.run(build_command, shell=True, check=True)
