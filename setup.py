6071a795bdd55087a88d74d8d4c8ff5e962789a2
Danderson123 <danielanderson1@hotmail.com>
2023-11-08 12:28:12 +0000

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,25 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# read the contents of your requirements file
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
 setuptools.setup(
     name="CRISPR-TAPE",
-    version="1.0.4",
+    version="1.0.5",
     author="Daniel Anderson",
-    author_email="",
+    author_email="danielanderson1@hotmail.com",
     license= "Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License",
     description="A protein-centric CRISPR gRNA design tool for TArgeted Protein Engineering",
     long_description="Existing CRIPSR gRNA design tools target protein-coding regions within genomic loci and non-specifically target the entire input region of DNA. Current tools fail to consider proteomic-based applications, so CRISPR-TAPE has been developed to reduce the substantial time burden associated with manual curation of gRNA libraries and empower the proteomics community." ,
     long_description_content_type="text/markdown",
     url="https://github.com/LaboratoryChild/CRISPR-TAPE",
+    install_requires=requirements,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.0',
+    python_requires='>=3.6',  # Update this to the minimum supported Python version
 )
